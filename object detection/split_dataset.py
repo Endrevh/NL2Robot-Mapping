834ae165d3804dea20e1ede56c574e6eebc5cb07
Endre Vee Hagestuen <endrevh@ntnu.no>
2024-04-23 15:06:17 +0200

    Clean directories before exporting datasets

--- object detection/split_dataset.py
+++ object detection/split_dataset.py
@@ -1,5 +1,7 @@
 import fiftyone as fo
 import fiftyone.utils.random as four
+import os
+import shutil
 
 # Load COCO formatted dataset
 coco_dataset = fo.Dataset.from_dir(
@@ -22,10 +24,16 @@ train_view = coco_dataset.match_tags("train")
 val_view = coco_dataset.match_tags("val")
 test_view = coco_dataset.match_tags("test")
 
-# Export the datasets
+# Export the datasets, clean directories if they already exist
 train_export_dir = "hammer_train"
 val_export_dir = "hammer_val"
 test_export_dir = "hammer_test"
 train_view.export(export_dir=train_export_dir, dataset_type=fo.types.COCODetectionDataset)
 val_view.export(export_dir=val_export_dir, dataset_type=fo.types.COCODetectionDataset)
+if os.path.exists(train_export_dir):
+    shutil.rmtree(train_export_dir)
+if os.path.exists(val_export_dir):
+    shutil.rmtree(val_export_dir)
+if os.path.exists(test_export_dir):
+    shutil.rmtree(test_export_dir)
 test_view.export(export_dir=test_export_dir, dataset_type=fo.types.COCODetectionDataset)
