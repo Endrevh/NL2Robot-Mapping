e7afe59980fa9059eebf4558dee883280c697317
Endre Vee Hagestuen <endrevh@ntnu.no>
2024-05-07 17:27:27 +0200

    Loop over combinations of hyperparameters

--- object detection/retrain.py
+++ object detection/retrain.py
@@ -1,29 +1,48 @@
 import tensorflow as tf
+import time
 assert tf.__version__.startswith('2')
 
 from mediapipe_model_maker import object_detector
 
-# Load the dataset
-train_dataset_path = "hammer_train"
-validation_dataset_path = "hammer_val"
+def retrain(train_data, validation_data, epochs, batch_size, learning_rate):
+    # Hyperparameters
+    spec = object_detector.SupportedModels.MOBILENET_MULTI_AVG_I384
+    hparams = object_detector.HParams(export_dir='exported_model', epochs=epochs, batch_size=batch_size, learning_rate=learning_rate)
+    options = object_detector.ObjectDetectorOptions(
+        supported_model=spec,
+        hparams=hparams
+    )
+
+    # Train the model
+    model = object_detector.ObjectDetector.create(
+        train_data=train_data,
+        validation_data=validation_data,
+        options=options)
+
+    return model
+
+# Load the datasets
+train_dataset_path = "train"
+validation_dataset_path = "val"
 train_data = object_detector.Dataset.from_coco_folder(train_dataset_path, cache_dir="/tmp/od_data/train")
 validation_data = object_detector.Dataset.from_coco_folder(validation_dataset_path, cache_dir="/tmp/od_data/validation")
-print("train_data size: ", train_data.size)
-print("validation_data size: ", validation_data.size)
 
-# Hyperparameters
-spec = object_detector.SupportedModels.MOBILENET_MULTI_AVG_I384
-hparams = object_detector.HParams(export_dir='exported_model')
-options = object_detector.ObjectDetectorOptions(
-    supported_model=spec,
-    hparams=hparams
-)
+# Measure time taken to train the model
+start = time.time()
 
-# Train the model
-model = object_detector.ObjectDetector.create(
-    train_data=train_data,
-    validation_data=validation_data,
-    options=options)
+# Loop over the different combinations of hyperparameters and save results
+for epochs in range(10, 110, 10):
+    for learning_rate in [0.1, 0.2, 0.3, 0.4, 0.5]:
+        model = retrain(train_data, validation_data, epochs=epochs, batch_size=8, learning_rate=learning_rate)
+        loss, coco_metrics = model.evaluate(validation_data, batch_size=4)
+        with open("training_results.txt", "a") as f:
+            f.write(f"{epochs},{learning_rate},{coco_metrics['AP']}\n")
+
+end = time.time()
+print(f"Training took {end - start} seconds")
+
+# Retrain the model
+# model = retrain(train_data, validation_data, epochs=10, batch_size=8, learning_rate=0.01)
 
 # Evaluate the model
 #loss, coco_metrics = model.evaluate(validation_data, batch_size=4)
@@ -31,4 +50,4 @@ model = object_detector.ObjectDetector.create(
 #print(f"Validation coco metrics: {coco_metrics}")
 
 # Export the model
-model.export_model()
+#model.export_model()
