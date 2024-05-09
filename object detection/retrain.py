f4e871661f3f989b0d84cedf69dfd33d0f2de623
Endre Vee Hagestuen <endrevh@ntnu.no>
2024-05-09 13:41:09 +0200

    New function: loop over hyperparameters

--- object detection/retrain.py
+++ object detection/retrain.py
@@ -21,22 +21,49 @@ def retrain(train_data, validation_data, epochs, batch_size, learning_rate):
 
     return model
 
+def loop_over_hyperparameters(train_data, validation_data, epochs, learning_rates):
+    # Loop over the different combinations of hyperparameters and save results
+    for epochs in range(10, 70, 10):
+        for learning_rate in [0.01, 0.02, 0.04, 0.06, 0.10, 0.15, 0.20, 0.30, 0.50]:
+            # Read from training_results.txt to avoid duplicate training
+            with open("training_results.txt", "r") as f:
+                skip = False
+                lines = f.readlines()
+                for line in lines:
+                    line = line.strip()
+                    if line == "":
+                        continue
+                    epoch = line.split(",")[0]
+                    lr = line.split(",")[1]
+                    if int(epoch) == epochs and float(lr) == learning_rate:
+                        skip = True
+                        break
+                # Skip training if already trained
+                if skip:
+                    print("Skipped training for this combination of hyperparameters: epochs=", epochs, ", learning_rate=", learning_rate)
+                    continue
+
+            print("Training for epochs=", epochs, ", learning_rate=", learning_rate)
+            model = retrain(train_data, validation_data, epochs=epochs, batch_size=8, learning_rate=learning_rate)
+            loss, coco_metrics = model.evaluate(validation_data, batch_size=4)
+            with open("training_results.txt", "a") as f:
+                f.write(f"{epochs},{learning_rate},{coco_metrics['AP']},{loss}\n")
+
 # Load the datasets
 train_dataset_path = "train"
 validation_dataset_path = "val"
 train_data = object_detector.Dataset.from_coco_folder(train_dataset_path, cache_dir="/tmp/od_data/train")
 validation_data = object_detector.Dataset.from_coco_folder(validation_dataset_path, cache_dir="/tmp/od_data/validation")
 
+# Select hyperparameters for loop_over_hyperparameters
+epochs = [10, 20, 30, 40, 50, 60]
+learning_rates = [0.01, 0.02, 0.04, 0.06, 0.10, 0.15, 0.20, 0.30, 0.50]
+
 # Measure time taken to train the model
 start = time.time()
 
-# Loop over the different combinations of hyperparameters and save results
-for epochs in range(10, 110, 10):
-    for learning_rate in [0.1, 0.2, 0.3, 0.4, 0.5]:
-        model = retrain(train_data, validation_data, epochs=epochs, batch_size=8, learning_rate=learning_rate)
-        loss, coco_metrics = model.evaluate(validation_data, batch_size=4)
-        with open("training_results.txt", "a") as f:
-            f.write(f"{epochs},{learning_rate},{coco_metrics['AP']}\n")
+# Loop over hyperparameters
+loop_over_hyperparameters(train_data, validation_data, epochs, learning_rates)
 
 end = time.time()
 print(f"Training took {end - start} seconds")
